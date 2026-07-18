ff1e167526e06eece2364256373d7fa50691796b
Evan Yan <evanyans@gmail.com>
2026-07-17 22:04:04 -0400

    Initial commit ios boilerpalte

---
+++ checkpointTests/checkpointTests.swift
@@ -0,0 +1,19 @@
+//
+//  checkpointTests.swift
+//  checkpointTests
+//
+//  Created by Evan Yan on 2026-07-17.
+//
+
+import Testing
+@testable import checkpoint
+
+struct checkpointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
